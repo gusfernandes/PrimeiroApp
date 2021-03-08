7553d587ba6affdf361379ec39faade76ac10fe1
gusfernandes <guga.spfc.guga@gmail.com>
2021-03-08 20:22:56 -0300

    Criação das actives

---
+++ app/src/main/java/com/example/primeiroapp/Cadastro_usuario_Activity.java
@@ -0,0 +1,14 @@
+package com.example.primeiroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cadastro_usuario_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_usuario);
+    }
+}
